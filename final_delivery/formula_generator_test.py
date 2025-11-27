8f67c9b30f8bf6bc91b65877801309bd4d58fe7d
Unarov Aital <unarov.aa@phystech.edu>
2025-11-27 10:12:33 +0300

    ..

--- final_delivery/formula_generator_test.py
+++ final_delivery/formula_generator_test.py
@@ -1,74 +1,80 @@
-import pytest
-from formula_generator import (
-    wavelength_to_band,
-    parse_satellite_bands_table,
-    convert_formula,
-)
-
-import pandas as pd
-
-landsat_bands = parse_satellite_bands_table("specification/Спутники.xlsx")[
-    "LANDSAT-89 OLITIRS"
-]
-
-emit_bands = parse_satellite_bands_table("specification/Спутники.xlsx")["EMIT"]
-
-
-@pytest.mark.parametrize(
-    "wl, expected_band",
-    [
-        (440, "SR_B1"),
-        (480, "SR_B2"),
-        (550, "SR_B3"),
-        (655, "SR_B4"),
-        (860, "SR_B5"),
-        (1600, "SR_B6"),
-        (2200, "SR_B7"),
-        (500, "SR_B2"),  # граница SR_B2
-        (670, "SR_B4"),  # граница SR_B4
-    ],
-)
-def test_single_wavelength(wl, expected_band):
-    assert wavelength_to_band(wl, wl, landsat_bands) == expected_band
-
-
-@pytest.mark.parametrize(
-    "wl_min, wl_max, expected_band",
-    [
-        (430, 450, "SR_B1"),  # полностью покрывает SR_B1
-        (450, 510, "SR_B2"),  # полностью покрывает SR_B2
-        (530, 590, "SR_B3"),  # полностью покрывает SR_B3
-        (640, 670, "SR_B4"),  # полностью покрывает SR_B4
-        (850, 880, "SR_B5"),  # полностью покрывает SR_B5
-        (500, 680, "SR_B8"),  # диапазон полностью попадает в панхроматический
-        (700, 710, "nan"),  # диапазон не покрыт ни одним мультиспектральным бэндом
-    ],
-)
-def test_wavelength_range(wl_min, wl_max, expected_band):
-    assert wavelength_to_band(wl_min, wl_max, landsat_bands) == expected_band
-
-
-@pytest.mark.parametrize(
-    "wl_min, wl_max, expected_band",
-    [
-        (377, 384, "B1"),  # диапазон полностью в B1
-        (386, 395, "B2"),  # диапазон B2
-        (400, 408, "B4"),  # перекрытие >50% с B4
-    ],
-)
-def test_emit_wavelength_range(wl_min, wl_max, expected_band):
-    assert wavelength_to_band(wl_min, wl_max, emit_bands) == expected_band
-
-
-@pytest.mark.parametrize(
-    "formula, expected_formula",
-    [
-        (
-            "([1600:1700] - [2145:2185])/([1600:1700] + [2145:2185])",
-            "(SR_B6 - SR_B7)/(SR_B6 + SR_B7)",
-        ),
-        ("(-485nm + 660nm)/660nm", "(-SR_B2 + SR_B4)/SR_B4"),
-    ],
-)
-def test_landsat_formula(formula, expected_formula):
-    assert convert_formula(formula, sat=landsat_bands) == expected_formula
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "65b1a2d9",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Не удалось спарсить лист: Обзор экспорта\n",
+      "Не удалось спарсить лист: Landsat-12345 MSS\n",
+      "Не удалось спарсить лист: Ресурс-П ГСА\n"
+     ]
+    }
+   ],
+   "source": [
+    "from formula_generator import convert_formula, parse_satellite_bands_table\n",
+    "import pandas as pd\n",
+    "\n",
+    "sat = parse_satellite_bands_table(\"specification/Спутники.xlsx\")\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "d848fe03",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "'SR_B8/SR_B8'"
+      ]
+     },
+     "execution_count": 3,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "f = \"[630:690]/[520:600]\"\n",
+    "\n",
+    "convert_formula(f, sat['LANDSAT-89 OLITIRS'])"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fa055f0f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": ".venv",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
